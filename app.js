a55185e9581b174fd021c983ae12e334d143550c
Kshitij Rajgude <kshitijrajgude@gmail.com>
2022-09-14 21:49:57 +0530

    create an input output system

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-var btnTranslate = document.querySelector("#btn-translate");
-console.log(btnTranslate);
-console.log("helo");
+let btnTranslate = document.querySelector(".btn-translate");
+let inputTranslate = document.querySelector(".input-translate");
+let output = document.querySelector(".output");
+
+function callBack(){
+    output.innerHTML = "dfklsdjf" + inputTranslate.value;
+
+}
+btnTranslate.addEventListener("click", callBack);
 
