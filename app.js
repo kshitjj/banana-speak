41432a52d1103be32e935191912980c46378c950
Kshitij Rajgude <kshitijrajgude@gmail.com>
2022-09-15 01:05:43 +0530

    API implementation

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 let btnTranslate = document.querySelector(".btn-translate");
 let inputTranslate = document.querySelector(".input-translate");
 let output = document.querySelector(".output");
+let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=";
 
-function callBack(){
-    output.innerHTML = "dfklsdjf" + inputTranslate.value;
+function getInputURL() {
+    let inputText = inputTranslate.value;
+    return (url + inputText)
+}
+
+function doFetch() {
+    fetch(getInputURL())
+        .then(response => response.json())
+        .then(json=> ((output.textContent = (json.contents.text)))); 
 
 }
-btnTranslate.addEventListener("click", callBack);
 
+
+btnTranslate.addEventListener("click",doFetch);
