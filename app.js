daa6b6d5af635d0921ad5111c0bc94cc06d75d51
Kshitij Rajgude <kshitijrajgude@gmail.com>
2022-09-15 03:52:15 +0530

    feat: added error catcher for APIs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let btnTranslate = document.querySelector(".btn-translate");
-let inputTranslate = document.querySelector(".input-translate");
+let btnTranslate = document.querySelector(".submit-btn");
+let inputTranslate = document.querySelector(".inputArea .textarea");
 let output = document.querySelector(".output");
 let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=";
 
@@ -8,11 +8,17 @@ function getInputURL() {
     return (url + inputText)
 }
 
+function errorHandler(error){
+    console.log("error occured " + error)
+    alert("There has been a problem with the server!");
+
+}
+
 function doFetch() {
     fetch(getInputURL())
         .then(response => response.json())
-        .then(json=> ((output.textContent = (json.contents.text)))); 
-
+        .then(json=> ((output.textContent = (json.contents.text))))
+        .catch(errorHandler)
 }
 
 
